1214e181a9ae2719fda4aa4a4a683389b546e56b
Kyle Headley <kyle.headley@colorado.edu>
2017-09-07 20:39:10 -0600

    Add polymorphic id

--- src/base.rs
+++ src/base.rs
@@ -51,3 +51,15 @@ impl<A:FuncType,B:FuncType<T2=A::T1>,X:Type<T=B::T1>> Func<X> for Comp<A,B> wher
 pub struct Const<T,V>(T,V);
 impl<T1,T2,V:Type<T=T2>> Type for Const<T1,V> {type T=Arrow<T1,T2>;}
 impl<T1,A:Type<T=T1>,T2,V:Type<T=T2>> Func<A> for Const<T1,V> {type F=V;}
+
+/// Polymorphic identity fn
+pub struct PolyFn;
+impl Type for PolyFn {type T=Arrow<Star,Star>;}
+impl<A:Type<T=Star>> Func<A> for PolyFn {type F=Arrow<A,A>;}
+pub struct IdFn<T>(T);
+impl<T> Type for IdFn<T> {type T=Arrow<T,T>;}
+impl<T,A:Type<T=T>> Func<A> for IdFn<T> {type F=A;}
+#[allow(non_camel_case_types)]
+pub struct id;
+impl PiType for id {type T1=Star; type F=PolyFn;}
+impl<A:Type<T=Star>> DFunc<A> for id {type D=IdFn<A>;}
