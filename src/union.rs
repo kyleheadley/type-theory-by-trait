283d6bb95e33f96df43931b5c6bfa55e24609d20
Kyle Headley <kyle.headley@colorado.edu>
2017-09-18 15:38:13 -0600

    Udpate sum constructors for uniformity

--- src/union.rs
+++ src/union.rs
@@ -15,11 +15,11 @@ pub struct Coproduct<T1:Typed,T2:Typed>(T1,T2);
 impl<A:Typed,B:Typed> Typed for Coproduct<A,B> {type T=Star;}
 
 /// Constructor for unions of left type
-pub struct Inl<A,T>(A,T);
-impl<T1:Typed,T2:Typed,A:Typed<T=T1>> Typed for Inl<A,T2> {type T=Coproduct<T1,T2>;}
+pub struct Inl<T1,T2,A>(T1,T2,A);
+impl<T1:Typed,T2:Typed,A:Typed<T=T1>> Typed for Inl<T1,T2,A> {type T=Coproduct<T1,T2>;}
 /// Constructor for unions of right type
-pub struct Inr<T,B>(T,B);
-impl<T1:Typed,T2:Typed,B:Typed<T=T2>> Typed for Inr<T1,B> {type T=Coproduct<T1,T2>;}
+pub struct Inr<T1,T2,B>(T1,T2,B);
+impl<T1:Typed,T2:Typed,B:Typed<T=T2>> Typed for Inr<T1,T2,B> {type T=Coproduct<T1,T2>;}
 
 // trial implementation
 /// (Curried) recursor for coproduct types 
@@ -38,9 +38,9 @@ impl<A:Typed,B:Typed,C:Typed,G0:Typed<T=Arrow<A,C>>> Typed for CoproductRecInner
 impl<A:Typed,B:Typed,C:Typed,G0:Typed<T=Arrow<A,C>>,G1:Typed<T=Arrow<B,C>>> Func<G1> for CoproductRecInner1<A,B,C,G0> {type F=CoproductRecInner2<A,B,C,G0,G1>;}
 pub struct CoproductRecInner2<A:Typed,B:Typed,C:Typed,G0:Typed<T=Arrow<A,C>>,G1:Typed<T=Arrow<B,C>>>(A,B,C,G0,G1);
 impl<A:Typed,B:Typed,C:Typed,G0:Typed<T=Arrow<A,C>>,G1:Typed<T=Arrow<B,C>>> Typed for CoproductRecInner2<A,B,C,G0,G1> {type T=Arrow<Coproduct<A,B>,C>;}
-impl<A:Typed,B:Typed,C:Typed,G0:Typed<T=Arrow<A,C>>,G1:Typed<T=Arrow<B,C>>,X:Typed<T=A>> Func<Inl<X,B>> for CoproductRecInner2<A,B,C,G0,G1> where
+impl<A:Typed,B:Typed,C:Typed,G0:Typed<T=Arrow<A,C>>,G1:Typed<T=Arrow<B,C>>,X:Typed<T=A>> Func<Inl<A,B,X>> for CoproductRecInner2<A,B,C,G0,G1> where
 	G0: Func<X>+FuncType<T1=A,T2=C>,
 {type F=<G0 as Func<X>>::F;}
-impl<A:Typed,B:Typed,C:Typed,G0:Typed<T=Arrow<A,C>>,G1:Typed<T=Arrow<B,C>>,X:Typed<T=B>> Func<Inr<A,X>> for CoproductRecInner2<A,B,C,G0,G1> where
+impl<A:Typed,B:Typed,C:Typed,G0:Typed<T=Arrow<A,C>>,G1:Typed<T=Arrow<B,C>>,X:Typed<T=B>> Func<Inr<A,B,X>> for CoproductRecInner2<A,B,C,G0,G1> where
 	G1: Func<X>+FuncType<T1=B,T2=C>,
 {type F=<G1 as Func<X>>::F;}
