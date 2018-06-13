e6ac1a4090c419f3ba5e11f5c1c220a4fe97ca49
Kyle Headley <kyle.headley@colorado.edu>
2018-06-13 14:43:37 -0600

    simplify, separate WF and typed checks

--- src/stlc/trial2.rs
+++ src/stlc/trial2.rs
@@ -1,66 +1,72 @@
-trait Type {}
+// Meta values and functions
+// -------------------------
 
-// Unit type
-struct Unit;
-impl Type for Unit {}
-
-// Bool type
-trait WFBool {}
-struct Bool;
-impl Type for Bool {}
-struct True;
-impl WFBool for True {}
+// Bool values
 struct False;
-impl WFBool for False {}
+struct True;
 
-// Nat type
-trait WFNat {}
-struct Nat;
-impl Type for Nat {}
+// Option values
+struct None;
+struct Some<V>(V);
+
+// Nat values
 struct Zero;
-impl WFNat for Zero {}
-struct Succ<N:WFNat>(N);
-impl<N:WFNat> WFNat for Succ<N> {}
+struct Succ<N>(N);
 
 // Nat equality function
-trait NatEq<N:WFNat> { type E:WFBool; }
-impl NatEq<Zero> for Zero { type E=True; }
-impl<N:WFNat> NatEq<Succ<N>> for Zero { type E=False; }
-impl<N:WFNat> NatEq<Zero> for Succ<N> { type E=False; }
-impl<N1:WFNat,N2:WFNat,B:WFBool> NatEq<Succ<N1>> for Succ<N2> where
-	N2: NatEq<N1,E=B>
-{ type E=B; }
+trait NatEq<N> { type Eq; }
+impl NatEq<Zero> for Zero { type Eq=True; }
+impl<N> NatEq<Succ<N>> for Zero { type Eq=False; }
+impl<N> NatEq<Zero> for Succ<N> { type Eq=False; }
+impl<N1,N2,E> NatEq<Succ<N1>> for Succ<N2> where
+	N2: NatEq<N1,Eq=E>
+{ type Eq=E; }
 
-// Arrow type
-trait WFArrow { type In:Type; type Out:Type; }
-struct Arrow<T1,T2>(T1,T2);
-impl<T1:Type,T2:Type> Type for Arrow<T1,T2> {}
-
-// Contexts
-trait Context {}
-struct Empty;
-impl Context for Empty {}
-struct Cxt<V,T,C:Context>(V,T,C);
-impl<N:WFNat,T:Type,C:Context> Context for Cxt<Var<N>,T,C> {}
+// Context
+struct EmptyCtx;
+struct TypeCtx<Var,Typ,Next>(Var,Typ,Next);
 
 // context membership function
-trait Contains<V> { type C:WFBool; type T:Type; }
-impl<N:WFNat> Contains<Var<N>> for Empty { type C=False; type T=Unit; }
-impl<N,N1,T,C,Eq,C2,T2> Contains<Var<N>> for Cxt<Var<N1>,T,C> where
-	N:WFNat,N1:WFNat,T:Type,C:Context,Eq:WFBool,C2:WFBool,T2:Type,
-	N1: NatEq<N,E=Eq>,
-	C: Contains2<Eq,T,Var<N>,C=C2,T=T2>,
-{ type C=C2; type T=T2; }
-trait Contains2<E,T,V> { type C:WFBool; type T:Type; }
-impl<T:Type,V,C:Context> Contains2<True,T,V> for C { type C=True; type T=T; }
-impl<T:Type,V> Contains2<False,T,V> for Empty { type C=False; type T=Unit; }
-impl<N,N1,T,T1,C1,Eq,C2,T2> Contains2<False,T,Var<N>> for Cxt<Var<N1>,T1,C1> where
-	N:WFNat,N1:WFNat,T:Type,T1:Type,C1:Context,Eq:WFBool,C2:WFBool,T2:Type,
-	N1: NatEq<N,E=Eq>,
-	C1: Contains2<Eq,T1,Var<N>,C=C2,T=T2>,
-{ type C=C2; type T=T2; }
+trait Contains<Val> { type Result; }
+impl<N> Contains<Var<N>> for EmptyCtx
+{ type Result=None; }
+
+impl<Check,First,Typ,Next,Eq,R>
+Contains<Var<Check>> for TypeCtx<Var<First>,Typ,Next> where
+	Check: NatEq<First,Eq=Eq>,
+	Next: Contains2<Eq,Typ,Var<Check>,Result=R>,
+{ type Result=R; }
+
+trait Contains2<Eq,Map,Check> { type Result; }
+
+impl<Map,C,Cxt> Contains2<True,Map,C> for Cxt
+{ type Result=Some<Map>; }
+
+impl<Map,C> Contains2<False,Map,C> for EmptyCtx
+{ type Result=None; }
+
+impl<Check,First,T,Typ,Next,Eq,R>
+Contains2<False,T,Var<Check>> for TypeCtx<Var<First>,Typ,Next> where
+	Check: NatEq<First,Eq=Eq>,
+	Next: Contains2<Eq,Typ,Var<Check>,Result=R>,
+{ type Result=R; }
 
 // Syntax
+// ------
+
+trait WFType {}
+
+struct Number;
+impl WFType for Number {}
+
+// we use our meta values in Num and Var below
+trait WFNat {}
+impl WFNat for Zero {}
+impl<N:WFNat> WFNat for Succ<N> {}
+
+struct Arrow<T1,T2>(T1,T2);
+impl<T1:WFType,T2:WFType> WFType for Arrow<T1,T2> {}
+
 trait Expr {}
 
 struct Num<N>(N);
@@ -73,59 +79,73 @@ struct Var<N>(N);
 impl<N:WFNat> Expr for Var<N> {}
 
 struct Lam<V,T,E>(V,T,E);
-impl<N:WFNat,T:Type,E:Expr> Expr for Lam<Var<N>,T,E> {}
+impl<N:WFNat,T:WFType,E:Expr> Expr for Lam<Var<N>,T,E> {}
 
 struct App<E1,E2>(E1,E2);
 impl<E1:Expr,E2:Expr> Expr for App<E1,E2> {}
 
 // Statics
-trait Typed<C:Context> { type T:Type; }
-impl<N:WFNat,C:Context> Typed<C> for Num<N> { type T=Nat; }
-impl<N1,N2,C> Typed<C> for Plus<N1,N2> where
-	N1:Typed<C,T=Nat>,N2:Typed<C,T=Nat>,C:Context,
-{ type T=Nat; }
-impl<N,C,T> Typed<C> for Var<N> where
-	N:WFNat,C:Context,T:Type,
-	C:Contains<Var<N>,C=True,T=T>
+trait Typed<Ctx> { type T; }
+impl<N,Ctx> Typed<Ctx> for Num<N> { type T=Number; }
+impl<N1,N2,Ctx> Typed<Ctx> for Plus<N1,N2> where
+	N1:Typed<Ctx,T=Number>,
+	N2:Typed<Ctx,T=Number>,
+{ type T=Number; }
+impl<N,Ctx,T> Typed<Ctx> for Var<N> where
+	Ctx:Contains<Var<N>,Result=Some<T>>
 { type T=T; }
-impl<C,N,T1,T2,E> Typed<C> for Lam<Var<N>,T1,E> where 
-	C:Context,N:WFNat,T1:Type,T2:Type,E:Expr,
-	E:Typed<Cxt<Var<N>,T1,C>,T=T2>,
+impl<Ctx,N,T1,T2,E> Typed<Ctx> for Lam<Var<N>,T1,E> where
+	E:Typed<TypeCtx<Var<N>,T1,Ctx>,T=T2>,
 { type T=Arrow<T1,T2>; }
-impl<C,E1,E2,T1,T2> Typed<C> for App<E1,E2> where
-	C:Context,E1:Expr,E2:Expr,T1:Type,T2:Type,
-	E1:Typed<C,T=Arrow<T1,T2>>,
-	E2:Typed<C,T=T1>
+impl<Ctx,E1,E2,T1,T2> Typed<Ctx> for App<E1,E2> where
+	E1:Typed<Ctx,T=Arrow<T1,T2>>,
+	E2:Typed<Ctx,T=T1>
 { type T=T2; }
 
 fn test(){
-	fn is_typed<E:Expr,T:Type>(e:E,t:T) where
-		E:Typed<Empty,T=T>
+	fn is_wf_expr<E:Expr>(e:&E) {}
+	fn is_wf_type<T:WFType>(t:&T) {}
+	fn is_typed<E,T>(e:&E,t:&T) where
+		E:Typed<EmptyCtx,T=T>
 	{}
 
+	struct Red;
+	let e = Plus(Num(Red),Num(Red));
+	let t = Number;
+	// is_wf_expr(&e); // fails
+	is_wf_type(&t);
+	is_typed(&e,&t); // typing Num doesn't rely on the actual nat
+
 	// λx.x
-	let e = Lam(Var(Zero),Nat,Var(Zero));
-	let t = Arrow(Nat,Nat);
-	is_typed(e,t);
-	// (λx.λy.y+x) 1 2
+	let e = Lam(Var(Zero),Number,Var(Zero));
+	let t = Arrow(Number,Number);
+	is_wf_expr(&e);
+	is_wf_type(&t);
+	is_typed(&e,&t);
+
+	// (λy.λx.x+y) 1 2
 	let e = App(App(
-		Lam(Var(Succ(Zero)),Nat,
-			Lam(Var(Zero),Nat,
+		Lam(Var(Succ(Zero)),Number,
+			Lam(Var(Zero),Number,
 				Plus(Var(Zero),Var(Succ(Zero)))
 			)
 		), Num(Succ(Zero))
 	), Num(Succ(Succ(Zero))));
-	let t = Nat;
-	is_typed(e,t);
+	let t = Number;
+	is_wf_expr(&e);
+	is_wf_type(&t);
+	is_typed(&e,&t);
 
-	// (λx.λy.y x) 2 (λx.1+x)
+	// (λy.λx.x y) 2 (λx.1+x)
 	let e = App(App(
-		Lam(Var(Succ(Zero)),Nat,
-			Lam(Var(Zero),Arrow(Nat,Nat),
+		Lam(Var(Succ(Zero)),Number,
+			Lam(Var(Zero),Arrow(Number,Number),
 				App(Var(Zero),Var(Succ(Zero)))
 			)
 		), Num(Succ(Succ(Zero)))
-	), Lam(Var(Zero),Nat,Plus(Num(Succ(Zero)),Var(Zero))));
-	let t = Nat;
-	is_typed(e,t);
+	), Lam(Var(Zero),Number,Plus(Num(Succ(Zero)),Var(Zero))));
+	let t = Number;
+	is_wf_expr(&e);
+	is_wf_type(&t);
+	is_typed(&e,&t);
 }
