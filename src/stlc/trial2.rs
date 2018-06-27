73e03b3c720dcb514bb3c2a7b08f769470b91e64
Kyle Headley <kyle.headley@colorado.edu>
2018-06-27 14:17:20 -0600

    stlc: add parser

--- src/stlc/trial2.rs
+++ src/stlc/trial2.rs
@@ -88,32 +88,86 @@ impl<E1:Expr,E2:Expr> Expr for App<E1,E2> {}
 // -------
 
 trait Typed<Ctx> { type T; }
+
 impl<N,Ctx> Typed<Ctx> for Num<N> { type T=Number; }
+
 impl<N1,N2,Ctx> Typed<Ctx> for Plus<N1,N2> where
 	N1:Typed<Ctx,T=Number>,
 	N2:Typed<Ctx,T=Number>,
 { type T=Number; }
+
 impl<N,Ctx,T> Typed<Ctx> for Var<N> where
 	Ctx:Contains<N,Result=Some<T>>
 { type T=T; }
+
 impl<Ctx,N,T1,T2,E> Typed<Ctx> for Lam<Var<N>,T1,E> where
 	E:Typed<TypeCtx<N,T1,Ctx>,T=T2>,
 { type T=Arrow<T1,T2>; }
+
 impl<Ctx,E1,E2,T1,T2> Typed<Ctx> for App<E1,E2> where
 	E1:Typed<Ctx,T=Arrow<T1,T2>>,
 	E2:Typed<Ctx,T=T1>
 { type T=T2; }
 
+// well-formedness checks
+fn is_wf_expr<E:Expr>(e:&E) {}
+fn is_wf_type<T:WFType>(t:&T) {}
+fn is_typed<E,T>(e:&E,t:&T) where
+	E:Typed<EmptyCtx,T=T>
+{}
+
+// Parsing
+// -------
+macro_rules! run {
+	($($x:tt)+) => (expr![$($x)*].code());
+}
+macro_rules! expr {
+	(($($e:tt)+)) => (expr![$($e)+]);
+	({^$e:expr}) => ($e);
+	(0) => (Num(Zero));
+	(1) => (Num(Succ(Zero)));
+	(2) => (Num(Succ(Succ(Zero))));
+	(3) => (Num(Succ(Succ(Succ(Zero)))));
+	(4) => (Num(Succ(Succ(Succ(Succ(Zero))))));
+	(5) => (Num(Succ(Succ(Succ(Succ(Succ(Zero)))))));
+	(6) => (Num(Succ(Succ(Succ(Succ(Succ(Succ(Zero))))))));
+	(7) => (Num(Succ(Succ(Succ(Succ(Succ(Succ(Succ(Zero)))))))));
+	(8) => (Num(Succ(Succ(Succ(Succ(Succ(Succ(Succ(Succ(Zero))))))))));
+	(9) => (Num(Succ(Succ(Succ(Succ(Succ(Succ(Succ(Succ(Succ(Zero)))))))))));
+	(a) => (Var(Zero));
+	(b) => (Var(Succ(Zero)));
+	(c) => (Var(Succ(Succ(Zero))));
+	(d) => (Var(Succ(Succ(Succ(Zero)))));
+	(e) => (Var(Succ(Succ(Succ(Succ(Zero))))));
+	(v) => (Var(Succ(Succ(Succ(Succ(Succ(Zero)))))));
+	(w) => (Var(Succ(Succ(Succ(Succ(Succ(Succ(Zero))))))));
+	(x) => (Var(Succ(Succ(Succ(Succ(Succ(Succ(Succ(Zero)))))))));
+	(y) => (Var(Succ(Succ(Succ(Succ(Succ(Succ(Succ(Succ(Zero))))))))));
+	(z) => (Var(Succ(Succ(Succ(Succ(Succ(Succ(Succ(Succ(Succ(Zero)))))))))));
+	(lam ($x:ident : $($t:tt)+)$(($($ts:tt)+))+ $($e:tt)+) => (Lam(
+		expr![$x],
+		typ![$($t)+],
+		expr![lam $(($($ts)+))+ $($e)+]
+	));
+	(lam ($x:ident : $($t:tt)+) $($e:tt)+) => (Lam(
+		expr![$x],
+		typ![$($t)+],
+		expr![$($e)+]
+	));
+	($n:tt + $($ns:tt)+) => (Plus(expr![$n],expr![$($ns)+]));
+	($a:tt $p:tt) => (App(expr![$a],expr![$p]));
+	($a:tt $p:tt $($ps:tt)+) => (expr![{^App(expr![$a],expr![$p])} $($ps),+]);
+}
+macro_rules! typ {
+	(($($ts:tt)+)) => (typ![$ts]);
+	(N) => (Number);
+	($t:tt -> $($ts:tt)+) => (Arrow(typ![$t],typ![$($ts)+]));
+}
+
 // Examples
 // --------
 
 fn test(){
-	fn is_wf_expr<E:Expr>(e:&E) {}
-	fn is_wf_type<T:WFType>(t:&T) {}
-	fn is_typed<E,T>(e:&E,t:&T) where
-		E:Typed<EmptyCtx,T=T>
-	{}
-
 	struct Red;
 	let e = Plus(Num(Red),Num(Red));
 	let t = Number;
@@ -122,34 +176,40 @@ fn test(){
 	is_typed(&e,&t); // typing Num doesn't rely on the actual nat
 
 	// λx.x
-	let e = Lam(Var(Zero),Number,Var(Zero));
-	let t = Arrow(Number,Number);
+	// let e = Lam(Var(Zero),Number,Var(Zero));
+	// let t = Arrow(Number,Number);
+	let e = expr![lam (x : N) x];
+	let t = typ![N -> N];
 	is_wf_expr(&e);
 	is_wf_type(&t);
 	is_typed(&e,&t);
 
 	// (λy.λx.x+y) 1 2
-	let e = App(App(
-		Lam(Var(Succ(Zero)),Number,
-			Lam(Var(Zero),Number,
-				Plus(Var(Zero),Var(Succ(Zero)))
-			)
-		), Num(Succ(Zero))
-	), Num(Succ(Succ(Zero))));
-	let t = Number;
+	// let e = App(App(
+	// 	Lam(Var(Succ(Zero)),Number,
+	// 		Lam(Var(Zero),Number,
+	// 			Plus(Var(Zero),Var(Succ(Zero)))
+	// 		)
+	// 	), Num(Succ(Zero))
+	// ), Num(Succ(Succ(Zero))));
+	// let t = Number;
+	let e = expr![(lam (y : N)(x : N) x+y) 1 2];
+	let t = typ![N];
 	is_wf_expr(&e);
 	is_wf_type(&t);
 	is_typed(&e,&t);
 
 	// (λy.λx.x y) 2 (λx.1+x)
-	let e = App(App(
-		Lam(Var(Succ(Zero)),Number,
-			Lam(Var(Zero),Arrow(Number,Number),
-				App(Var(Zero),Var(Succ(Zero)))
-			)
-		), Num(Succ(Succ(Zero)))
-	), Lam(Var(Zero),Number,Plus(Num(Succ(Zero)),Var(Zero))));
-	let t = Number;
+	// let e = App(App(
+	// 	Lam(Var(Succ(Zero)),Number,
+	// 		Lam(Var(Zero),Arrow(Number,Number),
+	// 			App(Var(Zero),Var(Succ(Zero)))
+	// 		)
+	// 	), Num(Succ(Succ(Zero)))
+	// ), Lam(Var(Zero),Number,Plus(Num(Succ(Zero)),Var(Zero))));
+	// let t = Number;
+	let e = expr![(lam (y:N)(x:N->N) x y) 2 (lam (x:N) 1+x)];
+	let t = typ![N];
 	is_wf_expr(&e);
 	is_wf_type(&t);
 	is_typed(&e,&t);
